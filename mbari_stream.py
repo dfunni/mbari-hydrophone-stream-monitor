9697ccada367d4b25c8c79bae443241851a7e649
dave <dfunni@gmail.com>
2020-08-20 11:15:55 -0700

    modified

--- mbari_stream.py
+++ mbari_stream.py
@@ -5,8 +5,9 @@ stream_url = 'http://streamingv2.shoutcast.com/ocean-soundscape?lang=en-us'
 r = requests.get(stream_url, stream=True)
 
 with open('whalesx.mp3', 'wb') as f:
-    try:
-        for block in r.iter_content(1024):
+
+    for block in r.iter_content(1024):
+        try:
             f.write(block)
-    except KeyboardInterrupt:
-        break 
+        except KeyboardInterrupt:
+            break
