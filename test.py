0f8525f4f72f1c07d306a5584a523d6dc4b1a705
dfunni <dfunni@gmail.com>
2020-12-29 13:36:49 -0800

    minor changes

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import utils
 import os
 
-#filename = 'whales'
-#tmp = filename + '.mp3'
-#out = filename + '.wav'
+filename = 'whales1'
+tmp = filename + '.mp3'
+out = filename + '.wav'
 #utils.mic_record(out, time_s=5, rate=44100)
-#utils.mbari_stream(tmp, duration_s=5)
-#utils.mp3_to_wav(tmp, out)
-#print("playing: ", out)
-#utils.play_wav(out)
+utils.mbari_stream(tmp, duration_s=5)
+utils.mp3_to_wav(tmp, out)
+print("playing: ", out)
+utils.play_wav(out)
 
 #_ = utils.plot_spectrogram(out)
-utils.stream_spec(chunk=1024, fs=48000)
+#utils.stream_spec(chunk=1024, fs=48000)
