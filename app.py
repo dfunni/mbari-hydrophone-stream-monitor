b76389eeef13cbb8ad8613e72d36b93c1f8178a7
David Funni <dfunni@gmail.com>
2023-09-24 03:42:53 +0000

    converted webapp to dash

--- app.py
+++ app.py
@@ -1,33 +1,64 @@
-from flask import Flask, render_template, request
+from dash import Dash, html, dcc, callback, Output, Input, State
+# import plotly.express as px
+from cetacean import DataDir, MarsClip, pull_data
 
-from cetacean import DataDir, MarsClip, get_data
+app = Dash(__name__)
 
-app = Flask(__name__)
+data = DataDir("/data/new_data/")
+datafile_iter = iter(data)
 
 
-@app.route("/")
-def plot():
-    # Generate the figure **without using pyplot**.
-    my_clip = MarsClip("/data/whale/20230920_002717Z.mp3")
-    data = my_clip.get_spec_img_data()
-    fname = my_clip.get_filename()
+app.layout = html.Div([
+    html.Div([
+        html.H1(id='title', children='MARS labler ', style={'textAlign':'center'}),
+        html.Img(id='graph-content'),
+    ]),
+    html.Div([
+        html.Audio(id='audio-control', controls=True) ## FIXME
+    ]),
+    html.Div([
+        html.Button('Whale', id='whale-val', n_clicks=0),
+        html.Button('No Whale', id='no_whale-val', n_clicks=0)
+    ]),
+    html.Div(id='whale-update', n_clicks=0, style={'display': 'none'}),
+    html.Div(id='no_whale-update', n_clicks=0, style={'display': 'none'}),
+    html.Div(id='filepath', style={'display': 'none'}),
+])
+
+@callback(
+    Output('graph-content', 'src'),
+    Output('title', 'children'),
+    Output('audio-control', 'src'),
+    Output('filepath', 'children'),
+    Input('whale-update', 'n_clicks'),
+    Input('no_whale-update', 'n_clicks')
+)
+def update_clip(whale_clicks, no_whale_clicks):
+    my_clip = MarsClip(next(datafile_iter))
+    fig_data = my_clip.get_spec_img_data()
+    # fname = my_clip.get_filename()
     fpath = my_clip.get_filepath()
-    return render_template('index.html', filename=fname, filepath=fpath, img_data=data)
+    title = f'MARS labler\n{fpath}'
+    fig_plt = f'data:image/png;base64,{fig_data}' 
+    return fig_plt, title, fpath, fpath
 
+@callback(
+    Output('whale-update', 'n_clicks'),
+    Input('whale-val', 'n_clicks'),
+    State('filepath', 'children')
+)
+def whale(n_clicks, filepath):
+    n_clicks += 1
+    return n_clicks
 
-@app.route("/", methods=['GET', 'POST'])
-def buttons():
-    if request.method == 'POST':
-        if request.form.get('whale') == 'Whale':
-            pass # do something
-        elif  request.form.get('no_whale') == 'No Whale':
-            pass # do something else
-        else:
-            pass # unknown
-    elif request.method == 'GET':
-        return render_template('index.html', form=form)
-    
-    return render_template("index.html")
+@callback(
+    Output('no_whale-update', 'n_clicks'),
+    Input('no_whale-val', 'n_clicks'),
+    State('filepath', 'children')
+)
+def no_whale(n_clicks, filepath):
+    n_clicks += 1
+    return n_clicks
 
 
 if __name__ == '__main__':
