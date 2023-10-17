c7908345d886ed46494c1119c1c1a63e791fef8e
David Funni <dfunni@gmail.com>
2023-10-17 03:54:34 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM "mcr.microsoft.com/devcontainers/python:13.11-bullseye"
+FROM "mcr.microsoft.com/devcontainers/python:1-3.11-bullseye"
 WORKDIR /app
 RUN apt-get update
 RUN apt-get install -y ffmpeg libsm6 libxext6 ssh-askpass
--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd /home/dfunni/mbari-hydrophone-stream-monitor/ 
 docker build --network=host -t mars-detect .
-docker run --name mars-detect -itd --network host --mount type=bind,source="$(pwd)",target=/app mars-detect
+docker run  -d \
+    -t \
+    --name mars-detect \
+    --network host \
+    --mount type=bind,source="$(pwd)",target=/app \
+    --mount type=bind,source="/home/dfunni/data",target=/app/MARS-detector/whales \
+    mars-detect
