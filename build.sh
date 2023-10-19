415d3a4bdcc125622499992f7c229d2b9d194fcd
David Funni <dfunni@gmail.com>
2023-10-19 17:02:37 +0000

    cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 cd /home/dfunni/mbari-hydrophone-stream-monitor/ 
 docker build --network=host -t mars-detect .
-docker run  -it -d \
-    --name mars-detect \
-    --network host \
-    --mount type=bind,source="$(pwd)",target=/app \
-    --mount type=bind,source="/home/dfunni/data",target=/app/data \
-    mars-detect ./start.sh
