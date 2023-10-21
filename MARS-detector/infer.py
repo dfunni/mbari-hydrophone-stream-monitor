3900ce5460520c82b815e3776af2aac912042250
David Funni <dfunni@gmail.com>
2023-10-21 17:23:07 +0000

    file cleanup

--- MARS-detector/infer.py
+++ MARS-detector/infer.py
@@ -1,47 +1,57 @@
 import warnings
 warnings.filterwarnings("ignore")
 
-import numpy as np
-import torch
-import yaml
-import os
 from argparse import ArgumentParser
+import yaml
+import logging
+
+import torch
 from torch import multiprocessing
-import torchaudio
 import torchaudio.transforms as T
 
-from mars_model import BinaryClassifier
-from mars_preprocess import preproccess
+from pipeline import preproccess, BinaryClassifier, send_email
 
 
 def main(filename, transform, net):
     with torch.no_grad():
         X = preproccess(filename, transform).unsqueeze(0)
         # calculate outputs by running images through the network
-        output = net(X)
-        if output.round().item():
-            call = 'whale'
-        else:
-            call = 'no whale'
+        try:
+            output = net(X)
+        except:
+            logging.error('inference error')
+        logging.debug(f'Inference output for {filename}: {output.item():.4f}')
         return output.round().int().item()
 
 
 if __name__ == "__main__":
+    
+    torch.set_default_tensor_type('torch.FloatTensor')
+    multiprocessing.set_start_method('spawn')
+
     parser = ArgumentParser()
     parser.add_argument('filename')
     args = parser.parse_args()
 
-    torch.set_default_tensor_type('torch.FloatTensor')
-    multiprocessing.set_start_method('spawn')
-
     with open('infer_config.yaml', 'r') as ymlfile:
-        config = yaml.load(ymlfile, Loader=yaml.Loader)
+        config = yaml.safe_load(ymlfile.read())
+
+    logging.basicConfig(format='%(asctime)s-%(process)d-%(levelname)s-%(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S', 
+                        level="DEBUG",
+                        filename='/workspaces/mbari-hydrophone-stream-monitor/MARS-detector/infer.log',
+                        filemode='a')
 
     net = BinaryClassifier()
     net.load_state_dict(torch.load(config['MODEL_ROOT'] + config['MODEL_NAME'] + '.pth',
                                    map_location=torch.device('cpu')))
 
-    transform = T.MelSpectrogram(n_mels=180, **config['torch_melspec_params']).cpu()
+    transform = T.MelSpectrogram(**config['melspec_params']).cpu()
   
     output = main(args.filename, transform, net)
+    if output:
+        send_email(subject='WHALE!',
+                   body='whale was detected',
+                   receiver='dfunni@gmail.com')
+    
     print(output)
---
+++ MARS-detector/pipeline/__init__.py
@@ -0,0 +1,4 @@
+from .mars_dataset import MARSDataset
+from .mars_preprocess import preproccess
+from .mars_model import BinaryClassifier
+from .mars_alert import send_email
