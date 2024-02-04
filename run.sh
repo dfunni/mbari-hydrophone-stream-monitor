b450bec69a7e07e5b0d771dab97ca18792254609
David Funni <dfunni@gmail.com>
2024-02-04 17:17:34 +0000

    changed audio segments on detection to 3, moved logging to /var/log

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ docker run  \
     --network host \
     --mount type=bind,source="$(pwd)",target=/workspaces/mbari-hydrophone-stream-monitor \
     --mount type=bind,source="/home/dfunni/data",target=/data \
+    --mount type=bind,source="/var/log",target=/var/log \
     mars-detect /workspaces/mbari-hydrophone-stream-monitor/start.sh
